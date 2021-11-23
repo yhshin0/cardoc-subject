82e717cb4195fdb77610f222137497fd6cb48b7f
yhshin0 <asyh1047@naver.com>
2021-11-23 23:49:11 +0900

    feat: config module 설정
    - app module에 config module import
    resolves: #3

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'cardoc.db',
