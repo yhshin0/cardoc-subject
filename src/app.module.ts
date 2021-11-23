ae0e489c683fe50f1d0e110ddec29b5b3026e019
yhshin0 <asyh1047@naver.com>
2021-11-23 22:21:35 +0900

    feat: auth module, controller, service 추가
    - auth module, controller, service 추가
    resolves: #3

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { UsersModule } from './users/users.module';
       synchronize: true,
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
