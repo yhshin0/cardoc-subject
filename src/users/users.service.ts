fec34fa42b343da08eaac9deb50c947e922dcbd1
yhshin0 <asyh1047@naver.com>
2021-11-23 20:52:01 +0900

    feat: ID 중복 체크
    - 회원 생성 시 ID 중복 체크 구현
    resolves: #2

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -10,7 +10,17 @@ export class UsersService {
     @InjectRepository(User) private readonly usersRepository: Repository<User>,
   ) {}
 
-  create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.usersRepository.findOne({
+      where: {
+        USER_USER_ID: createUserDto.user_id,
+      },
+    });
+
+    if (user) {
+      throw new BadRequestException('해당 ID가 이미 존재합니다');
+    }
+
     return this.usersRepository.save({
       USER_USER_ID: createUserDto.user_id,
       USER_PASSWORD: createUserDto.password,
