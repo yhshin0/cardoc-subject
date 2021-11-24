aad23cd92f038fcf2bb87c167cbeebf586fa53e3
yhshin0 <asyh1047@naver.com>
2021-11-24 09:18:21 +0900

    refactor: 회원 가입 API 이동
    - 회원 가입 API 변경(/users -> /auth/signup)
    resolves: #2, #11

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { User } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private usersService: UsersService,
+  ) {}
+
+  @Post('/signup')
+  async signup(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.usersService.create(createUserDto);
+  }
 
   @UseGuards(LocalAuthGuard)
   @Post('/signin')
