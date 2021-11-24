fa43776a8506c865e9fd61d6a627d0a51070613a
yhshin0 <asyh1047@naver.com>
2021-11-24 16:04:12 +0900

    feat: user, own-car 관계 설정
    - user, own-car 관계 설정
    resolves: #13

--- src/own-cars/entities/own-car.entity.ts
+++ src/own-cars/entities/own-car.entity.ts
@@ -1,1 +1,26 @@
-export class OwnCar {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class OwnCar {
+  @PrimaryGeneratedColumn()
+  OWN_CAR_ID: number;
+
+  @ManyToOne(() => User, (user) => user.ownCars, { eager: false })
+  @JoinColumn({ name: 'USER_ID' })
+  user: User;
+
+  @Column()
+  OWN_CAR_TRIM_ID: string;
+
+  @CreateDateColumn()
+  OWN_CAR_CREATED_AT: Date;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,12 +3,14 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { InternalServerErrorException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 
 import { USER_CONSTANTS } from '../constants/users.constants';
+import { OwnCar } from '../../own-cars/entities/own-car.entity';
 
 @Entity()
 export class User {
@@ -23,6 +25,9 @@ export class User {
 
   @CreateDateColumn()
   USER_CREATED_AT: Date;
+
+  @OneToMany(() => OwnCar, (ownCar) => ownCar.OWN_CAR_ID, { eager: false })
+  ownCars: OwnCar[];
 
   @BeforeInsert()
   async hashPassword(): Promise<void> {
