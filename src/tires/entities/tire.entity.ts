980f63df2fbc8a3eef6ceb314495b2b6cf96dc25
yhshin0 <asyh1047@naver.com>
2021-11-24 16:32:59 +0900

    feat: tire entity 정의
    - tire entity 정의
    resolves: #4

--- src/tires/entities/tire.entity.ts
+++ src/tires/entities/tire.entity.ts
@@ -1,4 +1,32 @@
-import { Entity } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { OwnCar } from '../../own-cars/entities/own-car.entity';
 
 @Entity()
-export class Tire {}
+export class Tire {
+  @PrimaryGeneratedColumn()
+  TIRE_ID: number;
+
+  @Column()
+  TIRE_WIDTH: number;
+
+  @Column()
+  TIRE_ASPECT_RATIO: number;
+
+  @Column()
+  TIRE_WHEEL_SIZE: number;
+
+  @CreateDateColumn()
+  TIRE_CREATED_AT: Date;
+
+  @OneToOne(() => OwnCar)
+  @JoinColumn({ name: 'OWN_CAR_ID' })
+  ownCar: OwnCar;
+}
