6ceb2a3d2d2e58de0d36b24a323289b168b83255
yhshin0 <asyh1047@naver.com>
2021-11-25 21:40:28 +0900

    refactor: 변수명 수정
    - OWN_CAR_ERROR_MESSAGE -> OWN_CAR_ERROR_MSG 로 변경
    resolves: #17

--- src/own-cars/constants/own-cars.constants.ts
+++ src/own-cars/constants/own-cars.constants.ts
@@ -1,3 +1,3 @@
-export const OWN_CAR_ERROR_MESSAGE = {
+export const OWN_CAR_ERROR_MSG = {
   NOT_EXIST_USER: '해당 유저가 존재하지 않습니다',
 };
--- src/own-cars/own-cars.service.ts
+++ src/own-cars/own-cars.service.ts
@@ -5,7 +5,7 @@ import { Repository } from 'typeorm';
 import { CreateOwnCarDto } from './dto/create-own-car.dto';
 import { OwnCar } from './entities/own-car.entity';
 import { UsersService } from '../users/users.service';
-import { OWN_CAR_ERROR_MESSAGE } from './constants/own-cars.constants';
+import { OWN_CAR_ERROR_MSG } from './constants/own-cars.constants';
 
 @Injectable()
 export class OwnCarsService {
@@ -21,7 +21,7 @@ export class OwnCarsService {
     const user = await this.usersService.findOne(user_id);
 
     if (!user) {
-      throw new BadRequestException(OWN_CAR_ERROR_MESSAGE.NOT_EXIST_USER);
+      throw new BadRequestException(OWN_CAR_ERROR_MSG.NOT_EXIST_USER);
     }
 
     return await this.ownCarsRepository.save(
